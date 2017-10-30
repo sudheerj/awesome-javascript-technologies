11836c3bca940d5b886292beece079ff3a1ab63d
sudheerj <jsudheer50@gmail.com>
2017-10-30 19:47:49 +0800

    Initiate Week-2 technology: Lodash-Utils methods

--- 02-Lodash/examples/util/02_bindall.js
+++ 02-Lodash/examples/util/02_bindall.js
@@ -5,6 +5,8 @@
 'use strict';
 var _ = require('../../node_modules/lodash');
 var jQuery = require('../../node_modules/jQuery');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
 var view = {
     'label': 'docs',
@@ -13,7 +15,11 @@ var view = {
     }
 };
 
-//var element = document.createElement("<div>");
+const dom = new JSDOM(`<body>
+  <script>document.body.appendChild(document.createElement("hr"));</script>
+</body>`);
+
+var element = dom.window.document.body.children[0];
 
 _.bindAll(view, ['click']);
 jQuery(element).on('click', view.click);
